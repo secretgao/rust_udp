b447506f50cc5b87bba1b6c69fbfd5820db340b9
891841626@qq.com <891841626@qq.com>
2022-03-15 14:36:15 +0800

    Edit

--- target/debug/deps/rust_udp-225188f067166661.d
+++
@@ -1,5 +0,0 @@
-/Users/gaoxinyu/work/rust_catalogue/rust_udp/target/debug/deps/rust_udp-225188f067166661: src/main.rs
-
-/Users/gaoxinyu/work/rust_catalogue/rust_udp/target/debug/deps/rust_udp-225188f067166661.d: src/main.rs
-
-src/main.rs:
--- target/debug/rust_udp.d
+++
@@ -1,1 +0,0 @@
-/Users/gaoxinyu/work/rust_catalogue/rust_udp/target/debug/rust_udp: /Users/gaoxinyu/work/rust_catalogue/rust_udp/src/main.rs
