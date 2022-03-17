5dc17fd8a6e3143a33ffcc1238606e2286e9f276
secretgao <891841626@qq.com>
2022-03-17 17:25:54 +0800

    队列实现栈

---
+++ examples/link_stack.rs
@@ -0,0 +1,45 @@
+//用链表实现栈
+#[derive(Debug)]
+struct StackNode<T>{
+    data:T,
+    next:Option<Box<StackNode<T>>>,
+}
+#[derive(Debug)]
+struct Stack<T>{
+    top:Option<Box<StackNode<T>>>,
+}
+
+impl <T> Stack<T> {
+    fn new()->Stack<T>{
+        Stack{ top:None }
+    }
+    fn push(&mut self,data:T){
+        let mut node = StackNode{data:data,next:None};
+        let next = self.top.take();
+        node.next = next;
+        self.top = Some(Box::new(node));
+    }
+    fn pop(&mut self) ->Option<T>{
+        let data = self.top.take();
+        match data {
+            Some(mut v)=>{
+                self.top = v.next.take();
+                Some(v.data)
+            },
+            None=>None,
+        }
+    }
+}
+fn main(){
+    let mut s = Stack::new();
+    s.push(1);
+    s.push(3);
+    s.push(5);
+    println!("s :{:#?}",s);
+    println!("================");
+    println!("s pop :{:#?}",s.pop());
+    println!("s pop :{:#?}",s.pop());
+    println!("s pop :{:#?}",s.pop());
+    println!("s pop :{:#?}",s.pop());
+    println!("s pop :{:#?}",s.pop());
+}
